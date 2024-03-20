604f1d6678a9415539b163fc725cc0cf72093e7b
Forrest Morrison <forrestmorrison@gmail.com>
2024-03-19 22:16:07 -0500

    added functionality to ImageSlider

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -16,8 +16,44 @@ const ImageSlider = ({ slides }) => {
         backgroundImage: `url(${slides[currentIndex].url})`
     }
 
+    const leftArrowStyles = {
+        position: 'absolute',
+        top: '40%',
+        transfor: 'translate(0, -50%)',
+        left: "32px",
+        fontSize: '45px',
+        color: '#fff',
+        zIndex: 1,
+        cursor: 'pointer'
+    }
+
+    const rightArrowStyles = {
+        position: 'absolute',
+        top: '40%',
+        transfor: 'translate(0, -50%)',
+        right: "32px",
+        fontSize: '45px',
+        color: '#fff',
+        zIndex: 1,
+        cursor: 'pointer'
+    }
+
+    const goToPrevious = () => {
+        const isFirstSlide = (currentIndex === 0)
+        const newIndex = (isFirstSlide ? slides.length - 1 : currentIndex - 1)
+        setCurrentIndex(newIndex);
+    }
+
+    const goToNext = () => {
+        const isLastSlide = (currentIndex === slides.length - 1)
+        const newIndex = (isLastSlide ? 0 : currentIndex + 1)
+        setCurrentIndex(newIndex);
+    }
+
     return (
         <div style={sliderStyles}>
+            <div style={leftArrowStyles} onClick={goToPrevious}>❬</div>
+            <div style={rightArrowStyles} onClick={goToNext}>❭</div>
             <div style={slideStyles}></div>
         </div>
     )
