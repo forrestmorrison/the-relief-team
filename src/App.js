7e3fb02b83f5449d485deedecaeae9c149fa2009
Forrest Morrison <forrestmorrison@gmail.com>
2024-03-19 19:06:18 -0500

    created react app, installed dependencies, created header with nav

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>The <strong>Relief</strong> Team</h1>
+        <ul className='nav'>
+          <li>About Us</li>
+          <li>Products</li>
+          <li>Services</li>
+          <li>Tools</li>
+          <li>Client Login</li>
+        </ul>
+      </header>
+      <main>
+
+      </main>
+    </div>
+  );
+}
+
+export default App;
