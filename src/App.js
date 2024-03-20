4bb83e97c2521d63844f9a62dc8110f775ca1ad5
Forrest Morrison <forrestmorrison@gmail.com>
2024-03-19 19:39:57 -0500

    stylized logo

--- src/App.js
+++ src/App.js
@@ -3,15 +3,23 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>The <strong>Relief</strong> Team</h1>
-        <ul className='nav'>
-          <li>About Us</li>
-          <li>Products</li>
-          <li>Services</li>
-          <li>Tools</li>
-          <li>Client Login</li>
-        </ul>
+      <header>
+        <div className='container'>
+          <div className='logo'>
+            <h1>The <strong>Relief</strong> Team</h1>
+            <div className='sub-text'>
+            <h4>Emergency Response</h4>
+            <h4>Pre-Planning & Support</h4>
+            </div>
+          </div>
+          <ul className='nav'>
+            <li>ABOUT US</li>
+            <li>PRODUCTS</li>
+            <li>SERVICES</li>
+            <li>TOOLS</li>
+            <li>CLIENT LOGIN</li>
+          </ul>
+        </div>
       </header>
       <main>
 
