1efbcb1e80a872cdaaa654ca0e0819a8dc1f46ca
Forrest Morrison <forrestmorrison@gmail.com>
2024-03-19 21:14:51 -0500

    created ImageSlider component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import './App.css';
+import Slide1 from "./images/slide-1.jpg";
+import Slide2 from "./images/slide-2.jpg";
+import Slide3 from "./images/slide-3.jpg";
 
 function App() {
   return (
@@ -26,7 +29,24 @@ function App() {
         </div>
       </header>
       <main>
-
+        <section className='slider'>
+          <div className='slides'>
+            <div id="slide-1">
+              <img src={Slide1} alt="" />
+            </div>
+            <div id="slide-2">
+            <img src={Slide2} alt="" />
+            </div>
+            <div id="slide-3">
+            <img src={Slide3} alt="" />
+            </div>
+          </div>
+          <div className='slide-tabs'>
+            <a href="#slide-1">•</a>
+            <a href="#slide-2">•</a>
+            <a href="#slide-3">•</a>
+          </div>
+        </section>
       </main>
     </div>
   );
